0fc47f6469abc4d7d3954fa0f4ae9a5f26c90634
Travis Mack <travismack47@protonmail.com>
2023-04-07 01:16:29 -0500

    added colors in css

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -50,6 +50,7 @@ for (let changes of supplyChanges) {
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
 
+
 // 9. We have a large stash of parts in our warehouse that we 
 //    need to box up and get ready for shipment. 
 //    There are 572 parts in total, and each box holds 7 parts.
